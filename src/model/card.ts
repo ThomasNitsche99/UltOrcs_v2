9e7eb5aac7f2fdab8b29fb05725b904a601123e4
Johan Solbakken <johsol@stud.ntnu.no>
2023-03-03 17:27:35 +0100

    add makeCardImageUrl

--- src/model/card.ts
+++ src/model/card.ts
@@ -8,16 +8,30 @@ export enum Suit {
 
 const suits = [Suit.Hearts, Suit.Diamonds, Suit.Clubs, Suit.Spades]
 
+/**
+ * Represents a card
+ */
 export type Card = {
     face: number,
     suit: Suit
 }
 
+/**
+ * createCard creates a new card
+ * @param face a number that represents 
+ * @param suit 
+ * @returns 
+ */
 export const createCard = (face: number, suit: Suit) => {
     // TODO: validate face and suit and throw if they are not right
     return { face, suit }
 }
 
+/**
+ * Deck class
+ * 
+ * Handles adding cards, drawing cards and shuffling of the deck.
+ */
 export class Deck {
     cards: Card[] = []
 
@@ -35,6 +49,10 @@ export class Deck {
     }
 }
 
+/**
+ * createDefaultDeck creates a standard deck consisting of all cards in a typical card deck
+ * @returns a deck
+ */
 export const createDefaultDeck = () => {
     const deck = new Deck()
 
@@ -47,14 +65,22 @@ export const createDefaultDeck = () => {
     return deck
 }
 
+/**
+ * createDeckFromCards creates a new deck from a list of cards.
+ * @param cards the list of cards
+ * @returns the deck
+ */
 export const createDeckFromCards = (cards: Card[]) => {
     const deck = new Deck();
     deck.cards = cards
     return deck
 }
 
-export const getCardImage = async (card: Card) => {
-    const url = `https://deckofcardsapi.com/static/img/${card.face}${card.suit as string}.png`
-    const response = await fetch(url)
-    return await response.json()
+/**
+ * makeCardImageUrl
+ * @param card the card
+ * @returns the image url for the card
+ */
+export const makeCardImageUrl = async (card: Card) => {
+    return `https://deckofcardsapi.com/static/img/${card.face}${card.suit as string}.png`
 }
