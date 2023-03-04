6bbdf1f18f38691908d77be4145fa7712b702ad3
Johan Solbakken <johsol@stud.ntnu.no>
2023-03-04 12:16:42 +0100

    Update test/cardImageTest

--- src/routes/test/cardImageTest/+page.svelte
+++ src/routes/test/cardImageTest/+page.svelte
@@ -1,40 +1,39 @@
 <script lang="ts">
-	import { createDeckFromCards, makeCardImageUrl, type Card } from '$lib/model/card';
+	import { createDefaultDeck, makeCardImageUrl, type Card } from '$lib/model/card';
 	import { Button, Img } from 'flowbite-svelte';
-	import type { PageData } from './$types';
 
-	export let data: PageData;
-	$:  deck = createDeckFromCards(data.cards);
+	let deck = createDefaultDeck();
 	let card: Card | null = null;
-
-	$: cards = data.cards;
+	$: counter = 0;
 </script>
 
-<div>
-	<div class="container flex justify-center ">
-		<Button
-			on:click={() => {
-				const newCard = deck.drawCard();
-				if (newCard !== undefined) {
-					card = newCard;
-					
-				}
-			}}
-		>
-			Draw card
-		</Button>
-		{#if card != null}
-			<div class="h-[400px]">
-				<Img src={makeCardImageUrl(card)} size="sm" class="" />
-			</div>
-		{/if}
-		<div />
-		<div class="container flex justify-center">
-			<div class="w-1/2">
-				{#each deck.cards as card}
+{#key counter}
+	<div>
+		<div class="container flex justify-center ">
+			<Button
+				on:click={() => {
+					const newCard = deck.drawCard();
+					if (newCard !== undefined) {
+						card = newCard;
+					}
+					counter++
+				}}
+			>
+				Draw card
+			</Button>
+			{#if card != null}
+				<div class="h-[400px]">
 					<Img src={makeCardImageUrl(card)} size="sm" class="" />
-				{/each}
+				</div>
+			{/if}
+			<div />
+			<div class="container flex justify-center">
+				<div class="w-1/2">
+					{#each [...Array(deck.cards.length).keys()].reverse() as i}
+						<Img src={makeCardImageUrl(deck.cards[i])} size="sm" class="" />
+					{/each}
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
+{/key}
