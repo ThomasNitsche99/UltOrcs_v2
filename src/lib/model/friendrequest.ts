c1fdaf3be4bcfa7f9eb8903e122951053aadaa21
Johan Solbakken <johsol@stud.ntnu.no>
2023-03-12 22:18:30 +0100

    Add friend request support
    Call GET /api/v1/friend/request to get a list of all friend requests you ahve sent or received.
    Call POST /api/v1/friend/request to send a friend request
    Call PUT /api/v1/friend/request/accept to accept a friend request.
    
    Accepting friend request currently not working. This is because I could not get friend list to work. This is a todo.
    
    To be implemented /api/v1/friend/list to get list of all friends.

---
+++ src/lib/model/friendrequest.ts
@@ -0,0 +1,9 @@
+
+export type PostFriendRequest = {
+    to: string,
+    from: string
+}
+
+export type PutFriendRequestAccept = {
+    friendRequestId: string
+}
---
+++ src/routes/api/v1/friend/list/+server.ts
@@ -0,0 +1,20 @@
+import { json } from "@sveltejs/kit";
+import type { RequestHandler } from "./$types";
+import { prisma } from "$lib/database";
+import type { User } from "@prisma/client";
+
+export const GET: RequestHandler = async ({ locals }) => {
+    const user = await prisma.user.findFirst({
+        where: {
+            username: (locals as { user: User }).user.username
+        }
+    })
+
+    if (user === null) {
+        return new Response(JSON.stringify({ error: "User does not exist" }), { status: 500 })
+    }
+
+    // TODO: Get firends list
+
+    return json("NOT IMPLEMENTED");
+}
